392dda4e56a67c8a05952dac52e56a628b296ce2
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-09-23 13:25:13 -0300

    Add maximize/minimize buttons to Editor and Previewer components; Change Editor and Previewer styles according to resizing

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import originalTextSelector from '../selectors/editorSelector'
-import { editText } from '../actions/creators'
+import { EDITOR, PREVIEWER, NONE } from '../utils/consts/maximizedState'
+import { editText, maximizeWindow } from '../actions/creators'
 import '../style/Editor.css'
 
 function Editor(props) {
@@ -9,16 +10,43 @@ function Editor(props) {
         props.editText(event.target.value)
     }
 
+    function handleResize() {
+        if (props.maximized === NONE)
+            props.maximizeWindow(EDITOR)
+        else if (props.maximized === EDITOR)
+            props.maximizeWindow(NONE)
+    }
+
+    function getResizeIcon() {
+        return props.maximized === EDITOR ? 'fas fa-compress-alt' : 'fas fa-expand-arrows-alt'
+    }
+
+    function getStyle() {
+        const style = {
+            wrapper: 'wrapper',
+            toolbar: 'toolbar',
+            editor: 'text-block editor-normal'
+        }
+
+        if (props.maximized === EDITOR) {
+            style.wrapper = 'wrapper-maximized'
+            style.toolbar = 'toolbar-maximized'
+            style.editor = 'text-block-maximized editor-maximized'
+        } else if (props.maximized === PREVIEWER) {
+            style.wrapper = 'wrapper-hidden'
+        }
+
+        return style
+    }
+
     return (
-        <div id='editor-wrapper' className='wrapper'>
-            <div id='editor-toolbar' className='toolbar'>
+        <div id='editor-wrapper' className={getStyle().wrapper}>
+            <div id='editor-toolbar' className={getStyle().toolbar}>
                 <i className='fab fa-free-code-camp toolbar-icon'></i>
                 <span className='toolbar-title'>Editor</span>
-                <i className='fas fa-expand-arrows-alt resize-button'></i>
+                <i className={getResizeIcon() + ' resize-button'} onClick={handleResize}></i>
             </div>
-            <textarea id='editor' className='text-block' onChange={handleInput} type='text' placeholder='Type here' autoFocus>
-                {props.originalText}
-            </textarea>
+            <textarea id='editor' className={getStyle().editor} onChange={handleInput} value={props.originalText} type='text' placeholder='Type here' autoFocus></textarea>
         </div>
     )
 }
@@ -27,6 +55,6 @@ function mapStateToProps(state) {
     return originalTextSelector(state)
 }
 
-const mapDispatchToProps = { editText }
+const mapDispatchToProps = { editText, maximizeWindow }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor)
--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import { connect } from 'react-redux'
 import parsedTextSelector from '../selectors/previewerSelector'
+import { EDITOR, PREVIEWER, NONE } from '../utils/consts/maximizedState'
+import { maximizeWindow } from '../actions/creators'
 import '../style/Previewer.css'
 
 function Previewer(props) {
@@ -10,14 +12,43 @@ function Previewer(props) {
         preview.current.innerHTML = props.parsedText
     })
 
+    function handleResize() {
+        if (props.maximized === NONE)
+            props.maximizeWindow(PREVIEWER)
+        else if (props.maximized === PREVIEWER)
+            props.maximizeWindow(NONE)
+    }
+
+    function getResizeIcon() {
+        return props.maximized === PREVIEWER ? 'fas fa-compress-alt' : 'fas fa-expand-arrows-alt'
+    }
+
+    function getStyle() {
+        const style = {
+            wrapper: 'wrapper preview-wrapper-normal',
+            toolbar: 'toolbar',
+            preview: 'text-block preview'
+        }
+
+        if (props.maximized === PREVIEWER) {
+            style.wrapper = 'wrapper-maximized'
+            style.toolbar = 'toolbar-maximized'
+            style.preview = 'text-block-maximized preview'
+        } else if (props.maximized === EDITOR) {
+            style.wrapper = 'wrapper-hidden'
+        }
+
+        return style
+    }
+
     return (
-        <div id='preview-wrapper' className='wrapper'>
-            <div id='preview-toolbar' className='toolbar'>
+        <div id='preview-wrapper' className={getStyle().wrapper}>
+            <div id='preview-toolbar' className={getStyle().toolbar}>
                 <i className='fab fa-free-code-camp toolbar-icon'></i>
                 <span className='toolbar-title'>Previewer</span>
-                <i className='fas fa-expand-arrows-alt resize-button'></i>
+                <i className={getResizeIcon() + ' resize-button'} onClick={handleResize}></i>
             </div>
-            <div id='preview' className='text-block' ref={preview}></div>
+            <div id='preview' className={getStyle().preview} ref={preview}></div>
         </div>
     )
 }
@@ -26,4 +57,6 @@ function mapStateToProps(state) {
     return parsedTextSelector(state)
 }
 
-export default connect(mapStateToProps)(Previewer)
+const mapDispatchToProps = { maximizeWindow }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Previewer)
